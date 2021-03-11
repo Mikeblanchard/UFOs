38a8ef152cc5c6c7ec2fa0b78b64f27d5c6b6747
Mikeblanchard <76125201+Mikeblanchard@users.noreply.github.com>
2021-03-11 15:01:09 -0500

    course work.

---
+++ course_work/1-HelloVariableWorld/hello-variable-world.js
@@ -0,0 +1,32 @@
+// Create a variable called "fullName" that holds a string
+var fullName = "Homer Simpson";
+// Create a variable called "country" that holds a string
+var country = "Canada";
+// Create a variable called "age" that holds an integer
+var age = 77;
+// Create a variable called "hourlyWage" that holds an integer
+var hourlyWage = 20;
+// Calculate the "dailyWage" for the user
+var dailyWage = hourlyWage * 8;
+// Create a variable that holds a number as a string
+var weeklyHours = 40;
+// Create a variable called 'weeklyWage' that converts a string into an integer
+var weeklyWage  = hourlyWage * parseInt(weeklyHours);
+// Create a variable called "satisfied" that holds a boolean
+var satisfied = true;
+// Print out "Hello <name>!"
+console.log(`Hello ${fullName}`); 
+// Print out what country the user entered
+console.log(`${country}`);
+// Print out the user's age
+console.log(`${age}`);
+// Print out the daily wage that was calculated
+console.log(`${dailyWage}`);
+// Print out the weekly wage that was calculated
+console.log(`${weeklyWage}`);
+// Using an IF statement to print out whether the users were satisfied
+if (satisfied === true) {
+    //  block of code to be executed if the condition is true
+  } else {
+    //  block of code to be executed if the condition is false
+  }
